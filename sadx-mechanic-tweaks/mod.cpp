81dfbde731e1c1c455837aabe1e7e1aa1af6ea13
Michael Fadely <michaelwfadely@gmail.com>
2017-12-05 22:52:55 -0700

    remove unused & use better (experimental) hook for onframe stuff

--- sadx-mechanic-tweaks/mod.cpp
+++ sadx-mechanic-tweaks/mod.cpp
@@ -1,4 +1,5 @@
 #include <SADXModLoader.h>
+#include <Trampoline.h>
 #include "EnemyBounceThing.h"
 #include "WaterRun.h"
 #include "ClassicJump.h"
@@ -13,95 +14,17 @@ PointerInfo jumps[] = {
 
 static const int jump_cancel = Buttons_B;
 
-namespace CollisionList
+static __int16 __cdecl RunSceneLogic_r();
+static Trampoline RunSceneLogic_t(0x00413D40, 0x00413D45, reinterpret_cast<void*>(&RunSceneLogic_r));
+static __int16 __cdecl RunSceneLogic_r()
 {
-	enum _enum
-	{
-		Players,
-		Unknown_1,
-		Targetable,
-		Hazard,
-		Unknown_4,
-		Unknown_5,
-		Minimal,
-		Rings,
-		Unknown_8,
-		Chao,
-		COUNT
-	};
+	const auto original = reinterpret_cast<decltype(RunSceneLogic_r)*>(RunSceneLogic_t.Target());
+	auto result = original();
+
+	ClassicRoll_OnFrame();
+
+	return result;
 }
-
-#pragma pack(push, 1)
-struct _CollisionData
-{
-	__int16 field_0;
-	__int16 field_2;
-	int field_4;
-	NJS_VECTOR v;
-	float anonymous_1;
-	float anonymous_2;
-	float anonymous_3;
-	int field_20;
-	int field_24;
-	int field_28;
-	int field_2C;
-};
-struct _CollisionInfo
-{
-	__int16 List;
-	__int16 field_2;
-	__int16 Flags;
-	__int16 Count;
-	float f8;
-	_CollisionData *CollisionArray;
-	Uint8 field_10[140];
-	ObjectMaster *Object;
-	__int16 field_A0;
-	__int16 field_A2;
-	_CollisionInfo *CollidingObject;
-};
-#pragma pack(pop)
-
-enum CollisionMode : Sint16
-{
-	Unknown00,
-	Unknown01,
-	Unknown02,
-	Solid,
-	Unknown04,
-	Unknown05,
-	Unknown06,
-	Unknown07,
-	Unknown08,
-	Unknown09,
-	CanTarget = 0x40
-};
-
-#if 0
-static void __cdecl InitCollision_(ObjectMaster *obj, _CollisionData *collisionArray, int count, Uint8 list);
-static Trampoline hax(0x0041CAF0, 0x0041CAF5, InitCollision_);
-static void __cdecl InitCollision_(ObjectMaster* obj, _CollisionData* collisionArray, int count, Uint8 list)
-{
-	FunctionPointer(void, original, (ObjectMaster*, _CollisionData*, int, Uint8), hax.Target());
-	original(obj, collisionArray, count, list);
-
-	//if (list != 6)
-		//return;
-
-	if (collisionArray == nullptr)
-		return;
-
-	//auto info = reinterpret_cast<_CollisionInfo*>(obj->Data1->CollisionInfo);
-	//if (info != nullptr)
-		//info->Flags = 0;
-
-	for (auto i = 0; i < count; i++)
-	{
-		//collisionArray[i].field_0 = 0;
-		collisionArray[i].field_2 = 0;
-	}
-}
-#endif
 
 extern "C"
 {
@@ -127,9 +50,5 @@ extern "C"
 
 		// Disable all dash panels
 		WriteData((char*)0x007A4450, 0xC3i8);
-	}
-	__declspec(dllexport) void __cdecl OnFrame()
-	{
-		ClassicRoll_OnFrame();
 	}
 }
